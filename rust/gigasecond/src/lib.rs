e86d303a5abb52138fd7a476c70e343ab3d8c3bf
vsoch <vsochat@stanford.edu>
2020-08-02 16:02:31 -0600

    adding leap exercise
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,4 +1,5 @@
-use chrono::*;
+use chrono::offset::TimeZone;
+use chrono::{DateTime, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
